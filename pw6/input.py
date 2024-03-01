a38eed4d1a502d4f686a8b24bfee76231a1b3252
Szonni <SZonny@proton.me>
2024-03-01 23:21:07 +0700

    pw6

---
+++ pw6/input.py
@@ -0,0 +1,66 @@
+
+from domains.Student import Student
+from domains.Course import Course
+import curses
+
+def input_student(stdscr, students):
+    curses.echo()
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Number of students is: ")
+    stdscr.refresh()
+    num = int(stdscr.getstr(1, 0).decode())
+    
+    for i in range (num):
+        stdscr.addstr(2, 0,f"Student number {i+1} | ID: ")
+        stdscr.refresh()
+        id = stdscr.getstr(3, 0).decode()
+
+        stdscr.addstr(4, 0, "Name: ")
+        stdscr.refresh()
+        name = stdscr.getstr(5, 0).decode()
+
+        stdscr.addstr(6, 0, "DOB: ")
+        stdscr.refresh()
+        dob = stdscr.getstr(7, 0).decode()
+
+        students.append(Student(id, name, dob))
+    curses.noecho()
+
+def input_course(stdscr, courses):
+    curses.echo()
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Number of courses: ")
+    stdscr.refresh()
+    num = int(stdscr.getstr(1, 0).decode())
+    for i in range (num):
+        stdscr.addstr(2, 0, f"Course number {i+1} | ID: ")
+        stdscr.refresh()
+        id = stdscr.getstr(3, 0).decode()
+
+        stdscr.addstr(4, 0, "Name: ")
+        stdscr.refresh()
+        name = stdscr.getstr(5, 0).decode()
+
+        courses.append(Course(id, name))
+    curses.noecho()
+
+def mark(stdscr, students, courses):
+    curses.echo()
+    stdscr.clear()
+    stdscr.addstr(0, 0, "///Mark input///")
+    stdscr.refresh()
+    stdscr.addstr(1, 0, "Enter course ID: ")
+    stdscr.refresh()
+    course_id = str(stdscr.getstr(2, 0).decode())
+    while True:    
+        if not any(course_id == course.get_id() for course in courses):
+            stdscr.addstr(3, 0, "Invalid course ID.")
+            stdscr.refresh()
+            stdscr.addstr(4, 0, "Please enter the correct course's ID: ")
+            stdscr.refresh()
+            course_id = str(stdscr.getstr(5, 0).decode())
+            continue
+        for student in students:
+            student.enter_mark(stdscr, course_id)
+        break
+    curses.noecho()
---
+++ pw6/main.py
@@ -0,0 +1,66 @@
+import curses
+import zipfile
+import pickle
+import os
+from input import input_student, input_course, mark
+from output import GPA, sort_GPA
+
+def save_data(filename, data):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
+
+def load_data(filename):
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
+
+def main(stdscr):
+    # Decompression
+    dat_file = "pw6/Info.dat"
+    output = "pw6"
+    if os.path.exists(dat_file):
+        with zipfile.ZipFile(dat_file, "r") as zip_ref:
+            zip_ref.extractall(output)
+
+    # Main
+    curses.curs_set(1)
+    courses = load_data("pw6/students.pickle") or []
+    students = load_data("pw6/courses.pickle") or []
+    
+    input_student(stdscr, students)
+    input_course(stdscr, courses)
+    mark(stdscr, students, courses)
+
+    GPA(stdscr, students)
+    # List students' infomation
+    stdscr.addstr(len(students) + 2, 0, str(students))
+    stdscr.refresh()
+    # List courses' information
+    stdscr.addstr(len(courses) + 4, 0, str(courses))
+    stdscr.refresh()
+
+    sorted_students = sort_GPA(students)
+    
+    stdscr.addstr(len(students) + len(courses) + 6, 0, "///Sorting GPA in Descending order.///")
+    stdscr.refresh()
+
+    for i, student in enumerate(sorted_students):
+        stdscr.addstr(i + len(students) + len(courses) + 8, 0, f"Name: {student.get_name()} | GPA: {student.get_GPA()}")
+        stdscr.refresh()
+    
+    stdscr.getch()
+
+    save_data("pw6/students.pickle", students)
+    save_data("pw6/courses.pickle", courses)
+
+    files = ["pw6/students.pickle", "pw6/courses.pickle"]
+    output_data = "pw6/Info.dat"
+
+    with zipfile.ZipFile(output_data, "w") as zip:
+        for file in files:
+            zip.write(file, os.path.basename(file))
+    for file in files:
+        os.remove(file)
+
+# Program start
+if __name__ == '__main__':
+    curses.wrapper(main)
